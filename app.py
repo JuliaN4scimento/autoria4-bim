ee02b413130f51da2548279d64558ad7b3518c32
Julia Nascimento <julia.nascimento1@escolar.ifrn.edu.br>
2026-01-22 09:25:12 -0300

    Inclusão das páginas html e conexão com o banco
    Inclusão de páginas HTML apenas com conteúdo inicial, conexão com o banco, criação de ambiente virtual e outros arquivos e pastas que iremos utilizar, teoricamente, no decorrer do projeto.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import mysql.connector
+
+app = Flask(__name__)
+
+cnx = mysql.connector.connect (
+    host = 'localhost',
+    user = 'root',
+    password = 'infoj',
+    database = 'mitoverso'
+)
+
+cursor = cnx.cursor()
+
